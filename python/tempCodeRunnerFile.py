072c64357fbc408f7083d525230717e3ace9d441
cuijiayu20 <2538452569@qq.com>
2022-11-24 13:45:06 +0800

    清理

--- python/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-title = i.group(1)
-            #     bv_id = ii.group(1)
-            #     mp4_url = 'https://www.bilibili.com/video/' + bv_id
-            #     print(str(count) + ' ' + title + '\n' + mp4_url)
-            #     bv_id_list.append(mp4_url)
-            #     print('\n')
-            #     count += 1
-            # yes_or_not = input('有想下载的视频吗？yes或者no :')
